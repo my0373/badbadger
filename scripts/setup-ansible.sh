2b0997636aee2e0f2c43c12e8d36d35d0090266c
Matt York <my0373@gmail.com>
2017-02-05 21:00:46 +0000

    Added some slightly erratic modules to get back in the swing of it

--- scripts/setup-ansible.sh
+++ scripts/setup-ansible.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 ## Script to setup Ansible on a Fedora machine.
 # Install ansible and Paramiko pre-reqs
-sudo dnf install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config python2-dnf
+sudo dnf install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config python2-dnf python-firewall
 # Install the paramiko libraries
 sudo pip install paramiko
 # Install Ansible itself
