2b452013362f995dcad07e563958c8b847a1e7ce
Matt York <my0373@gmail.com>
2017-02-05 17:40:22 +0000

    Created common playbook and fixed the ansible setup script.

---
+++ scripts/setup-ansible.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+## Script to setup Ansible on a Fedora machine.
+# Install ansible and Paramiko pre-reqs
+sudo dnf install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config python2-dnf
+# Install the paramiko libraries
+sudo pip install paramiko
+# Install Ansible itself
+sudo pip install ansible
